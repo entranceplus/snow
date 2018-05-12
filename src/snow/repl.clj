bd0851a1aaf202a4396c95f777eda4e2f8addc0f
Akash <ashakdwipeea@gmail.com>
2018-05-12 21:21:32 +0530

    merge and run multiple system as a single system

--- src/snow/repl.clj
+++ src/snow/repl.clj
@@ -1,5 +1,9 @@
 (ns snow.repl
   (:require [snow.systems :as sys]
+            [snow.env :refer [read-edn]]
+            [defn-spec.core :as ds]
+            [clojure.spec.alpha :as s]
+            [clojure.tools.deps.alpha.repl :refer [add-lib]]
             [snow.util :as u]))
 
 (def state (atom {}))
@@ -7,9 +11,25 @@
 (defn system []
   (-> @state ::sys/repl first))
 
-(defn start-systems [config]
-  (swap! state assoc ::sys/repl (sys/start-systems (u/make-vec-if-not config))))
+(ds/defn-spec start-systems
+  {::s/args (s/cat :config ::sys/systems)}
+  [config-map]
+  (println "Config map is " config-map)
+  (swap! state assoc ::sys/repl (sys/start-systems config-map)))
 
-(defn stop-systems []
+(defn stop! []
   (sys/stop-systems (get ::sys/repl @state)))
 
+(defn sys-map [f config]
+  {::sys/system-fn f
+   ::sys/config config})
+
+(defn start!
+  ([f]
+   (start! f (read-edn "profiles.edn")))
+  ([fn config]
+   (start-systems (if (coll? fn)
+                    (map #(-> % (sys-map config)) fn)
+                    (-> fn
+                       (sys-map config)
+                       u/make-vec-if-not)))))
--- src/snow/systems.clj
+++ src/snow/systems.clj
@@ -14,7 +14,7 @@
 
 (s/def ::system-def (s/keys :req [::system-fn ::config]))
 
-(s/def ::systems (s/coll-of ::system-def :kind vector?))
+(s/def ::systems (s/coll-of ::system-def))
 
 (s/def ::system-vars (s/coll-of ::system))
 
@@ -27,10 +27,9 @@
 (defn make-system-map [system-fn config prod?]
   (apply component/system-map (cond-> (system-fn config)
                                 prod? (conj :repl-server (-> :repl-port
-                                                             config
-                                                             read-string
-                                                             new-repl-server)))))
-
+                                                            config
+                                                            read-string
+                                                            new-repl-server)))))
 
 (defn gen-system
   ([system-config-gen-fn]
@@ -38,30 +37,26 @@
   ([system-config-gen-fn config & {:keys [prod?]}]
    (make-system-map system-config-gen-fn config prod?)))
 
-(s/fdef start-systems
-        :args (s/cat :systems ::systems)
-        :ret ::systems-vec)
+
+(defn- start-system [{:keys [snow.systems/system-fn snow.systems/config]} prod?]
+  (println "Starting a system with config " config " system-fn is " system-fn)
+  (if prod?
+    (safely (component/start (gen-system system-fn config :prod? prod?))
+            :on-error
+            :max-retry 20
+            :retry-delay [:random-exp-backoff :base 3000 :+/- 0.50]
+            :message (str "System start failed for config " config))
+    (component/start (gen-system system-fn config :prod? prod?))))
 
 (defn start-systems [systems & {:keys [prod?]}]
-  (doall (map (fn [{:keys [snow.systems/system-fn snow.systems/config]}]
-                (println "Starting a system with config " config " system-fn is " system-fn)
-                (if prod?
-                  (safely (component/start (gen-system system-fn config :prod? prod?))
-                          :on-error
-                          :max-retry 20
-                          :retry-delay [:random-exp-backoff :base 3000 :+/- 0.50]
-                          :message (str "System start failed for config " config))
-                  (component/start (gen-system system-fn config :prod? prod?))))
+  (doall (map #(-> % (start-system prod?))
               systems)))
-
-(s/fdef stop-system
-        :args (s/cat :systems ::systems-vec)
-        :ret ::systems-vec)
 
 (defn stop-systems [systems]
   (map #(component/stop %) systems))
 
-(defn get-port [config]
-  (let [port (config :http-port)]
-    (cond-> port
-      (string? port) Integer.)))
+(defn get-port
+  ([config] (get-port config :http-port))
+  ([config key] (let [port (config key)]
+                  (cond-> port
+                    (string? port) Integer.))))
